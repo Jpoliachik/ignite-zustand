d4a213fd2fb00dc1a58ae776d5bc48a4ea8d8bcd
Justin Poliachik <jpoliachik@gmail.com>
2024-02-01 11:47:59 -0500

    hookup zustand persist

--- app/app.tsx
+++ app/app.tsx
@@ -22,7 +22,6 @@ import { useFonts } from "expo-font"
 import React, { useEffect } from "react"
 import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
 import * as Linking from "expo-linking"
-// import { useInitialRootStore } from "./models"
 import { AppNavigator, useNavigationPersistence } from "./navigators"
 import { ErrorBoundary } from "./screens/ErrorScreen/ErrorBoundary"
 import * as storage from "./utils/storage"
@@ -30,6 +29,7 @@ import { customFontsToLoad } from "./theme"
 import Config from "./config"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { ViewStyle } from "react-native"
+import { useStore } from "./models"
 
 export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
 
@@ -71,19 +71,13 @@ function App(props: AppProps) {
 
   const [areFontsLoaded] = useFonts(customFontsToLoad)
 
-  // const { rehydrated } = useInitialRootStore(() => {
-  //   // This runs after the root store has been initialized and rehydrated.
-
-  //   // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to prevent flicker.
-  //   // Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
-  //   // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android Studio. Kill the app and launch it normally by tapping on the launcher icon. https://stackoverflow.com/a/69831106
-  //   // Note: (vanilla iOS) You might notice the splash-screen logo change size. This happens in debug/development mode. Try building the app for release.
-  //   setTimeout(hideSplashScreen, 500)
-  // })
+  const hasHydrated = useStore((state) => state._hasHydrated)
 
   useEffect(() => {
-    hideSplashScreen()
-  }, [])
+    if (hasHydrated) {
+      setTimeout(hideSplashScreen, 500)
+    }
+  }, [hasHydrated])
 
   // Before we show the app, we have to wait for our state to be ready.
   // In the meantime, don't render anything. This will be the background
@@ -91,7 +85,7 @@ function App(props: AppProps) {
   // In iOS: application:didFinishLaunchingWithOptions:
   // In Android: https://stackoverflow.com/a/45838109/204044
   // You can replace with your own loading component if you wish.
-  if (!isNavigationStateRestored || !areFontsLoaded) return null
+  if (!hasHydrated || !isNavigationStateRestored || !areFontsLoaded) return null
 
   const linking = {
     prefixes: [prefix],
--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,18 +1,43 @@
 import { create } from "zustand"
 import { useShallow } from "zustand/react/shallow"
+import { persist, createJSONStorage } from "zustand/middleware"
+
 import {
   AuthenticationStore,
   authenticationStoreSelector,
   createAuthenticationSlice,
 } from "./AuthenticationStore"
 import { EpisodeStore, createEpisodeSlice, episodeStoreSelector } from "./EpisodeStore"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
-export interface RootStore extends AuthenticationStore, EpisodeStore {}
+export interface RootStore extends AuthenticationStore, EpisodeStore {
+  _hasHydrated: boolean
+  setHasHydrated: (state: boolean) => void
+}
 
-export const useStore = create<RootStore>()((...a) => ({
-  ...createAuthenticationSlice(...a),
-  ...createEpisodeSlice(...a),
-}))
+export const useStore = create<RootStore>()(
+  persist(
+    (...a) => ({
+      ...createAuthenticationSlice(...a),
+      ...createEpisodeSlice(...a),
+
+      _hasHydrated: false,
+      setHasHydrated: (state) => {
+        const set = a[0]
+        set({
+          _hasHydrated: state,
+        })
+      },
+    }),
+    {
+      name: "zustand-app",
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => (state) => {
+        state?.setHasHydrated(true)
+      },
+    },
+  ),
+)
 
 export const useAuthenticationStore = () => useStore(useShallow(authenticationStoreSelector))
 export const useEpisodeStore = () => useStore(useShallow(episodeStoreSelector))
